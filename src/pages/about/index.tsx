fa7af4266d37106b306aa6253d5ad4c791495fb9
YourmixJNR <radmakson@gmail.com>
2025-02-08 14:00:53 +0100

    :cloud_with_lightning: feat: refactor About page to use centralized Intro component export

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,13 +1,10 @@
 import { SEO } from "../../components/global";
-import Intro from "./components/intro";
+import { Intro } from "./components";
 
 const About = () => {
   return (
     <>
       <SEO title="About" />
-      <div className="min-h-[100dvh] mx-auto flex flex-col items-center justify-center text-black text-[40px]">
-        About
-      </div>
       <Intro />
     </>
   );
